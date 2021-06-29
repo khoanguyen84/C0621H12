79e6527b0d0ec425084fa4a964be47f11fca637c
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2021-06-29 11:41:58 +0700

    Lession 6

---
+++ BCPreparation/Lession6/site.js
@@ -0,0 +1,7 @@
+function cong() {
+    let n1 = parseInt(document.getElementById('n1').value);
+    console.log(n1);
+    let n2 = parseInt(document.getElementById('n2').value);
+    console.log(n2);
+    document.getElementById('result').value = n1 + n2;
+}
